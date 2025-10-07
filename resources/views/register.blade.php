834b378fcc891ad893677cd565a704011c26c0ec
Tanut <148461669+Tanut-ph@users.noreply.github.com>
2025-10-07 22:25:48 +0700

    เพิ่มชั้นปี

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -132,11 +132,9 @@
         </select>
         <select name="year" class="Select-Box" required>
           <option selected>--- Select Year ---</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="erc">etc.</option>
+          @for($i = 1; $i <= 8; $i++)
+            <option value="{{ $i }}">{{ $i }}</option>
+          @endfor
         </select>
         <button type="submit">Sign up</button>
       </form>
